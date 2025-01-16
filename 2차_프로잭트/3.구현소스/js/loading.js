cd2cf826685f578cb0b792623f871bf342515434
Kangsoo01 <angelakang54@gmail.com>
2025-01-16 17:36:05 +0900

    테스트

--- 2차_프로잭트/3.구현소스/js/loading.js
+++ 2차_프로잭트/3.구현소스/js/loading.js
@@ -1,1 +1,15 @@
 // 로딩화면 JS - loading.js
+
+function counter() {
+  var count = setInterval(function () {
+    var c = parseInt($(".counter").text());
+    $(".counter").text((++c).toString());
+    if (c === 100) {
+      clearInterval(count);
+      $(".counter").addClass("hide");
+      $(".loading").addClass("active");
+    }
+  }, 10);
+}
+
+counter();

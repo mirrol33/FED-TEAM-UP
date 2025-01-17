59755995958f4aa4982c9cd5adc6614161947e1e
freelife11 <mirrol33@gmail.com>
2025-01-17 13:32:23 +0900

    01.17 update4

--- 2차_프로젝트/3.구현소스/js/basic.js
+++ 2차_프로젝트/3.구현소스/js/basic.js
@@ -8,32 +8,34 @@ import myFn from "./my_function.js";
 const headerComponent = {
   template: `
   <div class="header">
-        <h1><a href="./"><img src="./images/logo.png" alt="logo" /></a></h1>
-        <nav class="header-menu">
-          <ul>
-            <li><a href="./">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Story</a></li>
-            <li><a href="#">Video</a></li>
-            <li><a href="#">Products</a></li>
-          </ul>
-        </nav>
-        <div class="ham-nav-btn"><a href="">ham</a></div>
-        <div class="ham-nav-wrap open">
-          <div class="header__menu-circle n1"></div>
-          <div class="header__menu-circle n2"></div>
-          <div class="header__menu-circle n3"></div>
-          <div class="header__menu-circle n4"></div>
-          <div class="header__menu-circle n5"></div>
-          <ul class="ham-nav">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Story</a></li>
-            <li><a href="#">Video</a></li>
-            <li><a href="#">Products</a></li>
-          </ul>
-        </div>
-       </div>
+    <div class="inner">
+      <h1><a href="./"><img src="./images/logo.png" alt="logo" /></a></h1>
+      <div class="ham-nav-btn"><a href="#none"><svg data-v-f0103df8="" width="43" height="16" viewBox="0 0 43 16" fill="none" xmlns="http://www.w3.org/2000/svg"><rect data-v-f0103df8="" y="4" width="43" height="2" fill="#333" class="burger-line__top"></rect><rect data-v-f0103df8="" y="10" width="43" height="2" fill="#333" class="burger-line__bottom"></rect></svg></a></div>
+    </div>
+    <nav class="header-menu">
+      <ul>
+        <li><a href="./">Home</a></li>
+        <li><a href="#none">About</a></li>
+        <li><a href="#none">Story</a></li>
+        <li><a href="#none">Video</a></li>
+        <li><a href="#none">Products</a></li>
+      </ul>
+    </nav>
+    <div class="ham-nav-wrap open">
+      <div class="header__menu-circle n1"></div>
+      <div class="header__menu-circle n2"></div>
+      <div class="header__menu-circle n3"></div>
+      <div class="header__menu-circle n4"></div>
+      <div class="header__menu-circle n5"></div>
+      <ul class="ham-nav">
+        <li><a href="./">Home</a></li>
+        <li><a href="#none">About</a></li>
+        <li><a href="#none">Story</a></li>
+        <li><a href="#none">Video</a></li>
+        <li><a href="#none">Products</a></li>
+      </ul>
+    </div>
+    </div>
   `,
 };
 const footerComponent = {
@@ -52,3 +54,18 @@ new Vue({
     "footer-component": footerComponent,
   },
 });
+
+const headerMenu = myFn.qs('.ham-nav-btn');
+myFn.addEvt(window,'wheel',(e)=>{
+    let delta = e.wheelDelta;
+    // 스크롤 윈도우 top 값 구하기
+    let scrollTop = window.pageYOffset;
+    console.log(scrollTop);
+
+    // wheelDelta값이 마이너스는 아랫방향
+    // -> #header에 .on을 줘서 숨기기
+    if(delta < 0) headerMenu.classList.remove('on');
+    else headerMenu.classList.add('on');
+    if(scrollTop < 200) headerMenu.classList.remove('on');
+
+}); ///////// wheel 이벤트 함수 ////////

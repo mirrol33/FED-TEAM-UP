b2be85fcd74aa855a82c019910758d57e10fe0fc
freelife11 <mirrol33@gmail.com>
2025-01-17 15:24:35 +0900

    01.17 update6

--- 2차_프로젝트/3.구현소스/js/basic.js
+++ 2차_프로젝트/3.구현소스/js/basic.js
@@ -10,7 +10,14 @@ const headerComponent = {
   <div class="header">
     <div class="inner">
       <h1><a href="./"><img src="./images/logo.png" alt="logo" /></a></h1>
-      <div class="ham-nav-btn"><a href="#none"><svg data-v-f0103df8="" width="43" height="16" viewBox="0 0 43 16" fill="none" xmlns="http://www.w3.org/2000/svg"><rect data-v-f0103df8="" y="4" width="43" height="2" fill="#333" class="burger-line__top"></rect><rect data-v-f0103df8="" y="10" width="43" height="2" fill="#333" class="burger-line__bottom"></rect></svg></a></div>
+      <div class="ham-nav-btn">
+        <a href="#none">
+        <svg data-v-f0103df8="" width="43" height="16" viewBox="0 0 43 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect data-v-f0103df8="" y="4" width="43" height="2" fill="#333" class="burger-line__top"></rect>
+        <rect data-v-f0103df8="" y="10" width="43" height="2" fill="#333" class="burger-line__bottom"></rect>
+        </svg>
+        </a>
+      </div>
     </div>
     <nav class="header-menu">
       <ul>
@@ -63,21 +70,21 @@ new Vue({
   },
 });
 
-const headerMenu = myFn.qs('.ham-nav-btn');
-myFn.addEvt(window,'wheel',(e)=>{
-    let delta = e.wheelDelta;
-    // 스크롤 윈도우 top 값 구하기
-    let scrollTop = window.pageYOffset;
-    console.log(scrollTop);
+const headerMenu = myFn.qs(".ham-nav-btn");
+myFn.addEvt(window, "wheel", (e) => {
+  let delta = e.wheelDelta;
+  // 스크롤 윈도우 top 값 구하기
+  let scrollTop = window.pageYOffset;
+  console.log(scrollTop);
 
-    // wheelDelta값이 마이너스는 아랫방향
-    // -> #header에 .on을 줘서 숨기기
-    if(delta < 0) headerMenu.classList.remove('on');
-    else headerMenu.classList.add('on');
-    if(scrollTop < 200) headerMenu.classList.remove('on');
-
+  // wheelDelta값이 마이너스는 아랫방향
+  // -> #header에 .on을 줘서 숨기기
+  if (delta < 0) headerMenu.classList.remove("on");
+  else headerMenu.classList.add("on");
+  if (scrollTop < 200) headerMenu.classList.remove("on");
 }); ///////// wheel 이벤트 함수 ////////
 
-$('.ham-nav-btn').click(()=>{
-  $('.ham-nav-wrap').toggle();
+$(".ham-nav-btn").click(() => {
+  $(".ham-nav-wrap").toggle();
+  $(".ham-nav-btn").toggleClass("active");
 });

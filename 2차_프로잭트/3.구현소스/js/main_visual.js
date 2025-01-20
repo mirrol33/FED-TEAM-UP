40194b917da48d21b20f93e163ede566a6e40986
Kangsoo01 <angelakang54@gmail.com>
2025-01-20 18:00:35 +0900

    25-01-20 업데이트

---
+++ 2차_프로잭트/3.구현소스/js/main_visual.js
@@ -0,0 +1,8 @@
+function inforShowBox(){
+    // 제목
+    $("intro-text-box h2").css({opacity: 0}).delay(200).fadeTo(500,1)
+    // 설명
+    $("intro-text-box h2").css({opacity: 0}).delay(300).fadeTo(500,1)
+}
+
+inforShowBox();

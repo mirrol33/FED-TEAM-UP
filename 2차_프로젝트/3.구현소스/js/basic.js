11a598c55437a35a467c4fe9a90e55a4a4d789bc
freelife11 <mirrol33@gmail.com>
2025-01-20 17:18:49 +0900

    01.20 update2

--- 2차_프로젝트/3.구현소스/js/basic.js
+++ 2차_프로젝트/3.구현소스/js/basic.js
@@ -10,7 +10,7 @@ const headerComponent = {
   <div class="header">
     <div class="inner">
       <h1><a href="./"><img src="./images/logo.png" alt="logo" /></a></h1>
-      <div class="ham-nav-btn">
+      <div @click="toggleShow" class="ham-nav-btn">
         <a href="#none">
         <svg data-v-f0103df8="" width="43" height="16" viewBox="0 0 43 16" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect data-v-f0103df8="" y="4" width="43" height="2" fill="#333" class="burger-line__top"></rect>
@@ -21,38 +21,31 @@ const headerComponent = {
     </div>
     <nav class="header-menu">
       <ul>
-      <li v-for="(menu, index) in menus" :key="index">
+      <li v-for="(menu, idx) in menus">
         <a :href="menu.link">{{ menu.name }}</a>
       </li>
       </ul>
     </nav>
     <div class="ham-nav-wrap open">
-      <div class="header__menu-circle n1"></div>
-      <div class="header__menu-circle n2"></div>
-      <div class="header__menu-circle n3"></div>
-      <div class="header__menu-circle n4"></div>
-      <div class="header__menu-circle n5"></div>
+      <div v-for="n in 5" class="header__menu-circle" :class="'n' + n">
+    </div>
       <ul class="ham-nav">
-        <li @mouseover="addClass" @mouseout="removeClass" v-for="(menu, index) in menus" :key="index"><a :href="menu.link">{{ menu.name }}</a></li>
+        <li @mouseover="addClass" @mouseout="removeClass" v-for="(menu, idx) in menus"><a :href="menu.link">{{ menu.name }}</a></li>
         <li>
-          <a href="#none">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"/></svg>
-          </a>
-          <a href="#none">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
-          </a>
+          <a href="https://www.youtube.com/@pixar"><img src="./images/youtube_icon.svg"></a>
+          <a href="https://www.instagram.com/pixar"><img src="./images/instagram_icon.svg"></a>
         </li>
       </ul>
     </div>
     </div>
   `,
-  data:()=>({
+  data: () => ({
     menus: [
-      {name: "Home", link: "/"},
-      {name: "about", link: "/about"},
-      {name: "story", link: "/story"},
-      {name: "video", link: "/video"},
-      {name: "products", link: "/products"},
+      {name: "Home", link: "./"},
+      {name: "about", link: "./about.html"},
+      {name: "story", link: "./story.html"},
+      {name: "video", link: "./video.html"},
+      {name: "products", link: "./products.html"},
     ],
   }),
   methods: {
@@ -62,7 +55,7 @@ const headerComponent = {
       tg.classList.add("on");
       // 풍선이미지 추가
       let addSpan = document.createElement("span");
-      addSpan.innerHTML = `<img src="./images/Balloon.svg">`;
+      addSpan.innerHTML = `<img src="./images/Balloon_icon.svg">`;
       addSpan.className = "balloon";
       // 만약 .balloon 가 존재하면 추가 안함!
       if (!tg.querySelector(".balloon")) {
@@ -77,6 +70,17 @@ const headerComponent = {
       // 풍선이미지 삭제
       if (tg.querySelector(".balloon")) {
         tg.removeChild(addSpan);
+      }
+    },
+    toggleShow() {
+      this.show = !this.show;
+      const hamMenuWrap = myFn.qs(".ham-nav-wrap");
+      if(this.show){
+        //.ham-nav-wrap 요소에 .active 클래스 추가
+        hamMenuWrap.classList.add("active");
+      }else{
+        //.ham-nav-wrap 요소에.active 클래스 제거
+        hamMenuWrap.classList.remove("active");
       }
     },
   },
@@ -112,9 +116,4 @@ window.addEventListener("scroll", () => {
     headerMenu.classList.add("on");
   }
   preScrollTop = nextScrollTop;
-});
-
-$(".ham-nav-btn").click(() => {
-  $(".ham-nav-wrap").toggle();
-  $(".ham-nav-btn").toggleClass("active");
 });

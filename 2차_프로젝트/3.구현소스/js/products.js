27e4ea52b894d1dd087057df85bb28a6e7425f4e
freelife11 <mirrol33@gmail.com>
2025-02-06 12:51:01 +0900

    02.06 update1

--- 2차_프로젝트/3.구현소스/js/products.js
+++ 2차_프로젝트/3.구현소스/js/products.js
@@ -42,46 +42,84 @@ const proComponent = {
             {{ discountRate(p) }}%
           </span>
           <span class="inventory"> 재고수량: {{p.inventory}} </span>
+          <button @click="openSide(addToCart(p))">ADD TO CART</button>
         </a>
       </div>
     </div>
+    <!-- 장바구니 -->
+    <aside class="cart-list-aside">
+      <a href="#none" class="cart-toggle-btn" @click="closeSide()"><i class="fa-solid fa-cart-shopping"></i></a>
+      <h3>장바구니 목록</h3>
+      <div class="list">
+        <ul>
+          <li v-for="item in cart" :key="item.id">
+            <div class="thumbnail">
+              <img :src="'./images/products/' + item.id + '.png'" :alt="item.name" />
+            </div>
+            <div class="info">
+              <h4>{{ item.name }}</h4>
+              <p>판매가: {{ formatPrice(item.price) }} 원</p>
+              <p v-if="item.sale > 0">할인가: {{ formatPrice(item.sale) }} 원</p>
+              <p>수량: {{ item.quantity }}</p>
+            </div>
+            <button @click="removeFromCart(item.id)">삭제</button>
+          </li>
+        </ul>
+        <div class="total-price">
+          <p>총 결제 금액: {{ formatPrice(totalPrice) }} 원</p>
+          <button class="order-btn" type="button">주문하기</button>
+        </div>
+      </div>
+    </aside>
   </div>
   `,
   computed: {
-    // Vuex에서 상태(state) 가져오기
+    // Vuex에서 state 가져오기
     // selectedCategory(){return this.$store.state.selectedCategory}
-    ...Vuex.mapState(["selectedCategory"]),
-    
+    // cart(){return this.$store.state.cart}
+    ...Vuex.mapState(["selectedCategory", "cart"]),
+
     // Vuex에서 Getter 사용
     // filterProducts(){return this.$store.getters.filterProducts}
-    ...Vuex.mapGetters(["filterProducts"])
-
+    // totalPrice(){return this.$store.getters.totalPrice}
+    ...Vuex.mapGetters(["filterProducts", "totalPrice"]),
   },
   methods: {
     // Vuex의 Mutation 사용
     // setCategory(cate){this.$store.commit("setCategory", cate)}
-    ...Vuex.mapMutations(["setCategory"]),
-    
+    ...Vuex.mapMutations(["setCategory", "addToCart", "removeFromCart"]),
+
     selectCategory(cate, text) {
       this.setCategory(cate);
       document.querySelector(".select-cate").textContent = text;
     },
-    
+
     // 할인율 계산
     discountRate(product) {
-      return product.price > product.sale
-        ? Math.round(((product.price - product.sale) / product.price) * 100)
-        : 0;
+      return product.price > product.sale ? Math.round(((product.price - product.sale) / product.price) * 100) : 0;
     },
-    
+
     // 숫자에 콤마 추가
     formatPrice(price) {
       return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    },
+
+    openSide(){
+      if (!$('.cart-list-aside').hasClass('open')) {
+        $('.cart-list-aside').addClass('open');
+      }
+    },
+    closeSide(){
+      if (!$('.cart-list-aside').hasClass('open')) {
+        $('.cart-list-aside').addClass('open');
+      } else {
+        $('.cart-list-aside').removeClass('open');
+      }
     }
   },
-  mounted() {
+  action() {
     this.$store.dispatch("fetchProducts"); // 상품 데이터 불러오기
-  }
+  },
 };
 
 // proComponent 내보내기
--- 2차_프로젝트/3.구현소스/js/store.js
+++ 2차_프로젝트/3.구현소스/js/store.js
@@ -7,7 +7,8 @@ import proData from "../data/goods.json" with { type: "json" };
 export default new Vuex.Store({
   state: {
     products: proData,
-    selectedCategory: null
+    selectedCategory: null,
+    cart: [], // 장바구니 배열 추가
   },
   mutations: {
     setProducts(state, products) {
@@ -15,31 +16,31 @@ export default new Vuex.Store({
     },
     setCategory(state, category) {
       state.selectedCategory = category;
-    }
+    },
+    addToCart(state, product) {
+      console.log(state, product, product.id, product.inventory);
+      const itemInCart = state.cart.find((item) => item.id === product.id);
+      if (itemInCart) {
+        if (product.inventory > 0) 
+          itemInCart.quantity += 1;
+      } else {
+        state.cart.push({...product, quantity: 1});
+      }
+    },
+    removeFromCart(state, product) {
+      console.log(state, product, product.id);
+      state.cart = state.cart.filter((item) => item.id !== product);
+    },
   },
   getters: {
     filterProducts: (state) => {
       if (!state.selectedCategory) return state.products;
       return state.products.filter((product) => {
-        return Array.isArray(product.cate)
-          ? product.cate.includes(state.selectedCategory)
-          : product.cate === state.selectedCategory;
+        return Array.isArray(product.cate) ? product.cate.includes(state.selectedCategory) : product.cate === state.selectedCategory;
       });
-    }
-  }
+    },
+    totalPrice: (state) => {
+      return state.cart.reduce((sum, item) => sum + item.sale, 0);
+    },
+  },
 });
-
-
-// export default new Vuex.Store({
-//   state: proData,
-//   computed: {
-//     ...mapState(["selectedCategory"]),
-//     // selectedCategory(){return this.$store.state.selectedCategory}
-
-//     ...mapGetter(["filterProducts"])
-//     // filterProducts(){return this.$store.getters.filterProducts}
-//   }
-//   // mutations: {},
-//   // actions: {},
-//   // getters: {},
-// });
